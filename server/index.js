d482c8ffa882278f18a4dcc4a79a88fabd7d7f19
claudia <claudianataliurena@gmail.com>
2024-04-09 18:12:25 -0300

    post

--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
 const express = require("express");
 const app = express();
+
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "soporte",
+  database: "empleados_crud",
+});
+
+app.post("/create", (req, res) => {
+  const nombre = req.body.nombre;
+  const edad = req.body.edad;
+  const pais = req.body.pais;
+  const cargo = req.body.cargo;
+  const anios = req.body.anios;
+});
