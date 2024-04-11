67b6a8fbfbcf9b962744c769a472d46548d3bbdc
claudia <claudianataliurena@gmail.com>
2024-04-11 16:24:35 -0300

    insertando el get

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const mysql = require("mysql");
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3001, () => {
+  console.log("corriendo en el puerto 3001");
+});
 
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "soporte",
+  password: "password",
   database: "empleados_crud",
 });
 
@@ -16,8 +27,33 @@ app.post("/create", (req, res) => {
   const pais = req.body.pais;
   const cargo = req.body.cargo;
   const anios = req.body.anios;
-
+  console.log("create");
   db.query(
-    "INSERT INTO empleados(nombre,edad,pais,cargo,anios) VALUES (?,?,?,?,?)"
+    "INSERT INTO empleados(nombre,edad,pais,cargo,anios) VALUES (?,?,?,?,?)",
+    [nombre, edad, pais, cargo, anios],
+    (err, result) => {
+      if (err) {
+        console.log("ERROR DB", err);
+      } else {
+        console.log("REGISTRADO DB");
+        res.send("Empleado registrado con exito");
+      }
+    }
+  );
+});
+
+app.get("/empleados", (req, res) => {
+  console.log("get");
+  db.query(
+    "SELECT * FROM empleados",
+
+    (err, result) => {
+      if (err) {
+        console.log("ERROR DB get", err);
+      } else {
+        console.log("REGISTRADO DB get");
+        res.send("result");
+      }
+    }
   );
 });
