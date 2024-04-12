87680f799a8606f05eaddfa38194e652c1bdae3e
claudia <claudianataliurena@gmail.com>
2024-04-12 15:39:44 -0300

    empezando con el bootstrap en el cliente

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 
 import Axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [nombre, setNombre] = useState("");
@@ -21,6 +22,7 @@ function App() {
       anios: anios,
     })
       .then(() => {
+        getEmpleados();
         alert("Empleado registrado");
       })
       .catch((e) => alert(e));
@@ -29,72 +31,80 @@ function App() {
   const getEmpleados = () => {
     Axios.get("http://localhost:3001/empleados").then((response) => {
       setEmpleados(response.data);
-      alert("hola");
+      // alert("hola");
     });
   };
 
+  // getEmpleados();
+
   return (
-    <div className="App">
-      <div className="datos">
-        <label>
-          Nombre:
-          <input
-            onChange={(Event) => {
-              setNombre(Event.target.value);
-            }}
-            tipe="text"
-          ></input>{" "}
-        </label>
-        <br />
-        <label>
-          Edad:
-          <input
-            onChange={(Event) => {
-              setEdad(Event.target.value);
-            }}
-            tipe="number"
-          ></input>{" "}
-        </label>
-        <br />
-        <label>
-          Pais:
-          <input
-            onChange={(Event) => {
-              setPais(Event.target.value);
-            }}
-            tipe="text"
-          ></input>{" "}
-        </label>
-        <br />
-        <label>
-          Cargo:
-          <input
-            onChange={(Event) => {
-              setCargo(Event.target.value);
-            }}
-            tipe="text"
-          ></input>{" "}
-        </label>
-        <br />
-        <label>
-          Años:
-          <input
-            onChange={(Event) => {
-              setAnios(Event.target.value);
-            }}
-            tipe="number"
-          ></input>{" "}
-        </label>
-        <br />
-
-        <button onClick={add}>Registrar</button>
+    <div class="container">
+      <div className="App">
+        <div className="lista">
+          {empleadosList.map((val, key) => {
+            return <div className="">{val.nombre}</div>;
+          })}
+        </div>
       </div>
-      <div className="lista">
-        <button onClick={getEmpleados}>Listar</button>
-
-        {empleadosList.map((val, key) => {
-          return <div className="">{val.nombre}</div>;
-        })}
+      <div class="card text-center">
+        <div class="card-header">GESTION EMPLEADOS</div>
+        <div class="card-body">
+          <label>
+            Nombre:
+            <input
+              onChange={(Event) => {
+                setNombre(Event.target.value);
+              }}
+              tipe="text"
+            ></input>{" "}
+          </label>
+          <br />
+          <label>
+            Edad:
+            <input
+              onChange={(Event) => {
+                setEdad(Event.target.value);
+              }}
+              tipe="number"
+            ></input>{" "}
+          </label>
+          <br />
+          <label>
+            Pais:
+            <input
+              onChange={(Event) => {
+                setPais(Event.target.value);
+              }}
+              tipe="text"
+            ></input>{" "}
+          </label>
+          <br />
+          <label>
+            Cargo:
+            <input
+              onChange={(Event) => {
+                setCargo(Event.target.value);
+              }}
+              tipe="text"
+            ></input>{" "}
+          </label>
+          <br />
+          <label>
+            Años:
+            <input
+              onChange={(Event) => {
+                setAnios(Event.target.value);
+              }}
+              tipe="number"
+            ></input>{" "}
+          </label>
+          <br />
+        </div>
+        <div class="card-footer text-muted">
+          <button className="btn btn-success" onClick={add}>
+            Registrar
+          </button>
+        </div>
       </div>
     </div>
   );
