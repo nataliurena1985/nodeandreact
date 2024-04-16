5eec4e4108803e717fb03a329af2fd118516b996
claudia <claudianataliurena@gmail.com>
2024-04-15 22:59:49 -0300

    sweetAlert2 server try

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { useState } from "react";
 import Axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Swal from "sweetalert2";
+//const noti = withReactContent(Swal);
+
 function App() {
   const [nombre, setNombre] = useState("");
-  const [edad, setEdad] = useState(0);
+  const [edad, setEdad] = useState();
   const [pais, setPais] = useState("");
   const [cargo, setCargo] = useState("");
-  const [anios, setAnios] = useState(0);
+  const [anios, setAnios] = useState();
+  const [id, setId] = useState();
+
+  const [editar, setEditar] = useState(false);
   const [empleadosList, setEmpleados] = useState([]);
 
   const add = () => {
@@ -23,9 +29,46 @@ function App() {
     })
       .then(() => {
         getEmpleados();
-        alert("Empleado registrado");
+        limpiarCampos();
       })
       .catch((e) => alert(e));
+  };
+
+  const update = () => {
+    Axios.put("http://localhost:3001/update", {
+      id: id,
+      nombre: nombre,
+      edad: edad,
+      pais: pais,
+      cargo: cargo,
+      anios: anios,
+    })
+      .then(() => {
+        getEmpleados();
+        limpiarCampos();
+      })
+      .catch((e) => alert(e));
+  };
+
+  const limpiarCampos = () => {
+    setAnios("");
+    setNombre("");
+    setCargo("");
+    setEdad("");
+    setPais("");
+    setId("");
+    setEditar(false);
+  };
+
+  const editarEmpleado = (val) => {
+    setEditar(true);
+
+    setNombre(val.nombre);
+    setEdad(val.edad);
+    setCargo(val.cargo);
+    setPais(val.pais);
+    setAnios(val.anios);
+    setId(val.id);
   };
 
   const getEmpleados = () => {
@@ -40,7 +83,7 @@ function App() {
   return (
     <div className="container">
       <div className="card text-center">
-        <div class="card-header">GESTION EMPLEADOS</div>
+        <div className="card-header">GESTION EMPLEADOS</div>
         <div className="card-body">
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">
@@ -51,7 +94,8 @@ function App() {
               onChange={(Event) => {
                 setNombre(Event.target.value);
               }}
-              class="form-control"
+              className="form-control"
+              value={nombre}
               placeholder="Ingrese un Nombre"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -64,10 +108,11 @@ function App() {
             </span>
             <input
               type="number"
+              value={edad}
               onChange={(Event) => {
                 setEdad(Event.target.value);
               }}
-              class="form-control"
+              className="form-control"
               placeholder="Ingrese una edad"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -80,10 +125,11 @@ function App() {
             </span>
             <input
               type="text"
+              value={pais}
               onChange={(Event) => {
                 setPais(Event.target.value);
               }}
-              class="form-control"
+              className="form-control"
               placeholder="Ingrese un pais"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -96,10 +142,11 @@ function App() {
             </span>
             <input
               type="text"
+              value={cargo}
               onChange={(Event) => {
                 setCargo(Event.target.value);
               }}
-              class="form-control"
+              className="form-control"
               placeholder="Ingrese un cargo"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -112,23 +159,35 @@ function App() {
             </span>
             <input
               type="number"
+              value={anios}
               onChange={(Event) => {
                 setAnios(Event.target.value);
               }}
-              class="form-control"
+              className="form-control"
               placeholder="Ingrese los años"
               aria-label="Username"
               aria-describedby="basic-addon1"
             />
           </div>
         </div>
-        <div class="card-footer text-muted">
-          <button className="btn btn-success" onClick={add}>
-            Registrar
-          </button>
+        <div className="card-footer text-muted">
+          {editar ? (
+            <div>
+              <button className="btn btn-warning m-2" onClick={update}>
+                Actualizar
+              </button>
+              <button className="btn btn-info m-2" onClick={limpiarCampos}>
+                Cancelar
+              </button>
+            </div>
+          ) : (
+            <button className="btn btn-success" onClick={add}>
+              Registrar
+            </button>
+          )}
         </div>
       </div>
-      <table class="table table-striped">
+      <table className="table table-striped">
         <thead>
           <tr>
             <th scope="col">#</th>
@@ -137,11 +196,41 @@ function App() {
             <th scope="col">Pais</th>
             <th scope="col">Cargo</th>
             <th scope="col">Experiencia</th>
+            <th scope="col">Acciones</th>
           </tr>
         </thead>
         <tbody>
           {empleadosList.map((val, key) => {
-            return <div className="">{val.nombre}</div>;
+            return (
+              <tr key={val.id}>
+                <td>{val.id}</td>
+                <td>{val.nombre}</td>
+                <td>{val.edad}</td>
+                <td>{val.pais}</td>
+                <td>{val.cargo}</td>
+                <td>{val.anios}</td>
+                <td>
+                  <div
+                    className="btn-group"
+                    role="group"
+                    aria-label="Basic example"
+                  >
+                    <button
+                      type="button"
+                      onClick={() => {
+                        editarEmpleado(val);
+                      }}
+                      className="btn btn-info"
+                    >
+                      Editar
+                    </button>
+                    <button type="button" className="btn btn-danger">
+                      Eliminar
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
           })}
         </tbody>
       </table>
